864d448373ec987f0694c06fb219e361f235d6e4
nourah <altamiminourahcs@gmail.com>
2022-06-25 20:15:46 +0300

    v5

---
+++ script.js
@@ -0,0 +1,76 @@
+let imgs = ['img/horses/1.webp', 'img/horses/2.webp', 'img/horses/3.webp',
+ 'img/horses/4.webp', 'img/horses/5.webp', 'img/horses/6.webp', 'img/horses/1.webp',
+ 'img/horses/2.webp', 'img/horses/3.webp', 'img/horses/4.webp', 'img/horses/5.webp',
+ 'img/horses/6.webp'];
+	
+
+
+/* 
+
+
+var main = document.getElementsByClassName("main")[0],
+all_div = document.getElementsByTagName("div"),
+div,
+img;
+
+
+var audioRight = new Audio('sound/right.wav');
+var audioWrong = new Audio('sound/wrong.wav');
+
+var rArray = ['img/horses/1.webp', 'img/horses/2.webp', 'img/horses/3.webp', 'img/horses/4.webp', 'img/horses/5.webp', 'img/horses/6.webp', 'img/horses/1.webp', 'img/horses/2.webp', 'img/horses/3.webp', 'img/horses/4.webp', 'img/horses/5.webp', 'img/horses/6.webp'];
+
+function lettersFunction() {
+for (var i = 0; i <= 11; i = i + 1) {
+	div = document.createElement("div");
+	main.appendChild(div);
+	img = document.createElement("img");
+	img.setAttribute("src", rArray[i]);
+	img.setAttribute("id", rArray[i]);
+	div.appendChild(img);
+}
+
+}
+lettersFunction();
+
+
+
+var x = [],
+flag = true;
+for (var i = 0; i <= all_div.length - 1; i++) {
+all_div[i].onclick = function () {
+
+	if (!flag) return;
+	this.firstChild.style.opacity = "1";
+	if (x.length == 0) {
+		x[0] = this;
+	} else if (x.length == 1) {
+		x[1] = this;
+
+	}
+
+	if (x.length == 2) {
+		flag = false;
+		setTimeout(check, 700);
+
+	}
+
+};
+
+}
+
+function check() {
+
+if (x[0].firstChild.id === x[1].firstChild.id) {
+	audioRight.play();
+	//alert("Okay, Great go to the next ones");
+} else {
+	audioWrong.play();
+	//alert("Sorry, Not identcical try agin");         
+	x[0].firstChild.style.opacity = "0";
+	x[1].firstChild.style.opacity = "0";
+
+}
+x = [];
+flag = true;
+}
+ */
