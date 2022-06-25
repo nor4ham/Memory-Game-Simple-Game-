13a9b34a6525085f8aa7d79d8ed69fbd7c776e81
nourah <altamiminourahcs@gmail.com>
2022-06-26 00:11:03 +0300

    v6

--- script.js
+++ script.js
@@ -2,9 +2,49 @@ let imgs = ['img/horses/1.webp', 'img/horses/2.webp', 'img/horses/3.webp',
  'img/horses/4.webp', 'img/horses/5.webp', 'img/horses/6.webp', 'img/horses/1.webp',
  'img/horses/2.webp', 'img/horses/3.webp', 'img/horses/4.webp', 'img/horses/5.webp',
  'img/horses/6.webp'];
+ let main=document.getElementById("main")	
+
+imgs.forEach(function (item) {
+    main.innerHTML+='<div class="horses"><img class="horseImage"src="'+item+'"></div>';
+});
+const horses = document.querySelectorAll(".horseImage")
+flg = true;
+let checkArry = [];
+let counter =0;
+horses.forEach(hores => {
+	hores.addEventListener('click', function handleClick(event) {
+		if(flg){
+			hores.setAttribute('style', 'opacity: 1;');
+			if(checkArry.length==0){
+				checkArry[0]=hores;
+			}
+			else if (checkArry.length==1){
+				checkArry[1]=hores;
+			}
+		   if(checkArry.length==2){
+				flg = false;
+				setTimeout(check, 700);
+			}
 	
-
-
+		}
+	else{return}
+	
+	});
+  });
+  function check(){
+	if(checkArry[0].src==checkArry[1].src){
+		alert("goof")
+		counter++;
+		let score=document.getElementById("score")	
+		score.innerHTML=counter;
+	  
+	}
+	else{ 
+		checkArry[0].setAttribute('style', 'opacity: 0;');
+		checkArry[1].setAttribute('style', 'opacity: 0;');}
+	flg = true;
+	checkArry=[];	
+  }
 /* 
 
 
@@ -42,6 +82,7 @@ all_div[i].onclick = function () {
 	if (!flag) return;
 	this.firstChild.style.opacity = "1";
 	if (x.length == 0) {
+		var x = [],
 		x[0] = this;
 	} else if (x.length == 1) {
 		x[1] = this;
